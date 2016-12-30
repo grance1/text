e19d56a6b866db33d3222347befdd77bc4b4d903
grance1 <2397580855@qq.com>
2016-12-30 19:15:50 +0800

    update this file

--- baike_spider/html_outputer.py
+++ baike_spider/html_outputer.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python2
 # -*- coding: UTF-8 -*-
+import MySQLdb
+
+
+
 class HtmlOutputer(object):
     def __init__(self):
         self.datas = []
@@ -15,12 +19,26 @@ class HtmlOutputer(object):
         fout.write("<body>")
         fout.write("<table>")
 
+
+        conn = MySQLdb.connect(host="127.0.0.1", user="root", passwd='123', db="db_house_price")
+        cursor = conn.cursor()
+
+
         for data in self.datas:
             fout.write("<tr>")
             fout.write("<td>%s</td>" % data['url'])
             fout.write("<td>%s</td>" % data['title'].encode('utf-8'))
             fout.write("<td>%s</td>" % data['summary'].encode('utf-8'))
             fout.write("</tr>")
+
+            cursor.execute('insert into test1 (url,title,summary) values (%s, %s,%s)',(data['url'].encode('utf-8'),data['title'].encode('utf-8'),data['summary'].encode('utf-8')))
+
+
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+
         fout.write("</table>")
         fout.write("</body>")
         fout.write("</html>")
